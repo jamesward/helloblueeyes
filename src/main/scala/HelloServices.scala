92c34feb35a008ac4e694afbd82ff1ff78542a66
Mike Slinn <mslinn@micronauticsresearch.com>
2011-10-11 12:58:49 -0700

    Created IDEA project file

--- src/main/scala/HelloServices.scala
+++ src/main/scala/HelloServices.scala
@@ -3,19 +3,20 @@ import blueeyes.concurrent.Future
 import blueeyes.core.http.{HttpRequest, HttpResponse}
 import blueeyes.core.http.MimeTypes._
 import blueeyes.core.http.combinators.HttpRequestCombinators
-import blueeyes.core.data.BijectionsChunkString
-
+import blueeyes.core.data.{BijectionsChunkString, ByteChunk}
+import blueeyes.core.service.{HttpService, HttpServiceContext}
 
 trait HelloServices extends BlueEyesServiceBuilder with HttpRequestCombinators with BijectionsChunkString {
-    val hello = service("hello", "0.1") { context =>
-        request {
-            path("/") {
+    val hello:HttpService[ByteChunk] = service("hello", "0.1") { context:HttpServiceContext =>
+        request/*:AsyncHttpService[ByteChunk]*/ {
+            path("/")/*:HttpServices.HttpService[ByteChunk, Future[HttpResponse[ByteChunk]]]*/ {
                 produce(text/html) {
                     get { request =>
                         val content = <html>
                                          <body>hello, world</body>
                                       </html>
                         val response = HttpResponse[String](content = Some(content.buildString(true)))
+                        println(response);
                         Future.sync(response)
                      }
                 }
