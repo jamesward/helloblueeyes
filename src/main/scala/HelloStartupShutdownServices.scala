28fd1a9c9e2c40d6f081641ad35f2af42e2a1e08
Mike Slinn <mslinn@micronauticsresearch.com>
2011-10-13 13:45:36 -0700

    Corrected indentation

--- src/main/scala/HelloStartupShutdownServices.scala
+++ src/main/scala/HelloStartupShutdownServices.scala
@@ -5,9 +5,6 @@ import blueeyes.core.http.MimeTypes._
 import blueeyes.core.http.combinators.HttpRequestCombinators
 import blueeyes.core.data.{BijectionsChunkString, ByteChunk}
 
-import net.lag.logging.Logger
-
-
 trait HelloStartupShutdownServices extends BlueEyesServiceBuilder with HttpRequestCombinators with BijectionsChunkString {
   
     val helloStartupShutdown= service("helloHtml", "0.1") { context =>
@@ -17,20 +14,20 @@ trait HelloStartupShutdownServices extends BlueEyesServiceBuilder with HttpReque
         } ->
         request {
           println("request")
-            path("/hello") {
-              println("path /")
-                produce(text/html) {
-                  println("produce html")
-                    get { request: HttpRequest[ByteChunk] =>
-                      println("get")
-                        val content = <html>
-                                         <body>hello, world</body>
-                                      </html>
-                        val response = HttpResponse[String](content = Some(content.buildString(true)))
-                        Future.sync(response)
-                     }
-                }
+          path("/hello") {
+            println("path /")
+            produce(text/html) {
+              println("produce html")
+              get { request: HttpRequest[ByteChunk] =>
+                println("get")
+                val content = <html>
+                                 <body>hello, world</body>
+                              </html>
+                val response = HttpResponse[String](content = Some(content.buildString(true)))
+                Future.sync(response)
+              }
             }
+          }
         } ->
         shutdown {
           println("shutdown")
