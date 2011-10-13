ae871864a776540be70934929db1282ee97b7d0f
Mike Slinn <mslinn@micronauticsresearch.com>
2011-10-13 14:36:57 -0700

    Using Scala default of 2 spaces per tab

--- src/main/scala/HelloStartupShutdownServices.scala
+++ src/main/scala/HelloStartupShutdownServices.scala
@@ -5,33 +5,34 @@ import blueeyes.core.http.MimeTypes._
 import blueeyes.core.http.combinators.HttpRequestCombinators
 import blueeyes.core.data.{BijectionsChunkString, ByteChunk}
 
-trait HelloStartupShutdownServices extends BlueEyesServiceBuilder with HttpRequestCombinators with BijectionsChunkString {
-  
-    val helloStartupShutdown= service("helloHtml", "0.1") { context =>
-        startup {
-          println("startup")
-          Future.sync(())
-        } ->
-        request {
-          println("request")
-          path("/hello") {
-            println("path /")
-            produce(text/html) {
-              println("produce html")
-              get { request: HttpRequest[ByteChunk] =>
-                println("get")
-                val content = <html>
-                                 <body>hello, world</body>
-                              </html>
-                val response = HttpResponse[String](content = Some(content.buildString(true)))
-                Future.sync(response)
-              }
-            }
+trait HelloStartupShutdownServices extends BlueEyesServiceBuilder
+  with HttpRequestCombinators with BijectionsChunkString {
+
+  val helloStartupShutdown= service("helloHtml", "0.1") { context =>
+    startup {
+      println("startup")
+      Future.sync(())
+    } ->
+    request {
+      println("request")
+      path("/hello") {
+        println("path /")
+        produce(text/html) {
+          println("produce html")
+          get { request: HttpRequest[ByteChunk] =>
+            println("get")
+            val content = <html>
+                             <body>hello, world</body>
+                          </html>
+            val response = HttpResponse[String](content = Some(content.buildString(true)))
+            Future.sync(response)
           }
-        } ->
-        shutdown {
-          println("shutdown")
-          Future.sync(())
         }
+      }
+    } ->
+    shutdown {
+      println("shutdown")
+      Future.sync(())
     }
+  }
 }
