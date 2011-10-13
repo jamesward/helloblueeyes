d95901ef1fa3f25eda197a30075e40eff41ffb3a
Mike Slinn <mslinn@micronauticsresearch.com>
2011-10-13 14:27:19 -0700

    Using logging instead of println

--- src/main/scala/HelloJsonServices.scala
+++ src/main/scala/HelloJsonServices.scala
@@ -1,27 +1,27 @@
 import blueeyes._
 import blueeyes.concurrent.Future
+import blueeyes.core.data.{BijectionsChunkJson, BijectionsChunkString, ByteChunk}
 import blueeyes.core.http.{HttpRequest, HttpResponse}
-import blueeyes.core.http.MimeTypes._
 import blueeyes.core.http.combinators.HttpRequestCombinators
-import blueeyes.core.data.{BijectionsChunkString, ByteChunk}
 import blueeyes.core.service.{HttpService, HttpServiceContext}
 import blueeyes.json.JsonAST._
-import blueeyes.core.data.{ByteChunk, BijectionsChunkJson}
-import blueeyes.persistence.mongo.{ConfigurableMongo, MongoFilterAll, Mongo, MongoFilter}
 
 trait HelloJsonServices extends BlueEyesServiceBuilder
-  with HttpRequestCombinators with BijectionsChunkString
+  with HttpRequestCombinators
+  with BijectionsChunkString
   with BijectionsChunkJson {
     val helloJson:HttpService[ByteChunk] = service("helloJson", "0.1") {
-      context:HttpServiceContext =>
-      request {
-        path("/json") {
-          jvalue {
-            get { requestParam:HttpRequest[JValue] =>
-               val json = JString("Hello World!")
-               val response = HttpResponse[JValue](content = Some(json))
-               println(response);
-               Future.sync(response)
+      logging { log =>
+        context:HttpServiceContext =>
+        request {
+          path("/json") {
+            jvalue {
+              get { requestParam:HttpRequest[JValue] =>
+                 val json = JString("Hello World!")
+                 val response = HttpResponse[JValue](content = Some(json)) 
+                 log.info(response.toString())
+                 Future.sync(response)
+              }
             }
           }
         }
