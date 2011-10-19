3fc436652f6053f082928d638563c5c626404201
Ubuntu Desktop 64 bit 11.04 <mslinn@natty.(none)>
2011-10-19 15:39:11 -0700

    LetterProbabilities passes unit tests

--- src/main/scala/net/interdoodle/hbe/domain/Monkey.scala
+++ src/main/scala/net/interdoodle/hbe/domain/Monkey.scala
@@ -1,21 +1,7 @@
 package net.interdoodle.hbe.domain
 
-import collection.mutable.HashMap
-import scala.Array
-
 /** Random or semi-random typist
  * @author Mike Slinn */
 
-class Monkey(letters:IndexedSeq[Char]=('A' to 'Z') ++ ('a' to 'z') ++ ('0' to '9') /*++ ('.>,</?'";:[{]}\|=+-_\)\(*&^%$#@!~`')*/,
-             weights:Array[Double]=new Array[Double](0)) {
-  var letterWeightMap = new HashMap[Char, Double]
-  var weightArray = weights
-  if (weights.length==0) { // all letters should have equal weight
-    for (i <- 0 to  letters.length)
-      weightArray(i) = 1.0 / letters.length;
-  } else { // custom weighting was supplied
-    for (letter <- letters)
-      for (weight <- weights)
-        letterWeightMap.put(letter, weight)
-  }
+class Monkey {
 }
--- src/test/scala/net/interdoodle/hbe/HelloBlueEyesSuite.scala
+++ src/test/scala/net/interdoodle/hbe/HelloBlueEyesSuite.scala
@@ -1,23 +1,82 @@
 package net.interdoodle.hbe
 
-import net.interdoodle.hbe.domain._
+import net.interdoodle.hbe.domain.LetterProbabilities
 import org.scalatest.FunSuite
-import org.scalatest.BeforeAndAfterEach
+
 
 /** @see http://www.scalatest.org/scaladoc/1.6.1/#org.scalatest.FunSuite */
 class HelloBlueEyesSuite extends FunSuite {
-  var monkey:Monkey = _
+  var lps:LetterProbabilities = _
 
   test("default constructor values") {
-    val letters:IndexedSeq[Char] = ('A' to 'Z') ++ ('a' to 'z') ++ ('0' to '9')
-    val weights:Array[Double] = new Array[Double](letters.length)
     try {
-      monkey = new Monkey(letters, weights)
+      lps = new LetterProbabilities()
+
+      lps.add('a')
+      assert(lps.size==1)
+      lps.computeValues()
+      assert(lps.normalizedValues.size==1)
+      assert(lps.normalizedValues.head._1=='a')
+      assert(lps.normalizedValues.head._2==1.0)
+      assert(lps.normalizedValues.last._1=='a')
+      assert(lps.normalizedValues.last._2==1.0)
+      assert(lps.continuousValues.size==1)
+      assert(lps.continuousValues.head._1=='a')
+      assert(lps.continuousValues.head._2==1.0)
+      assert(lps.letter(-123)=='a')
+      assert(lps.letter(0)=='a')
+      assert(lps.letter(0.2)=='a')
+      assert(lps.letter(1)=='a')
+      assert(lps.letter(123)=='a')
+
+      lps.add('b')
+      assert(lps.size==2)
+      lps.computeValues()
+      assert(lps.normalizedValues.size==2)
+      assert(lps.normalizedValues.head._1=='a')
+      assert(lps.normalizedValues.head._2==0.5)
+      assert(lps.normalizedValues.last._1=='b')
+      assert(lps.normalizedValues.last._2==0.5)
+      assert(lps.continuousValues.size==2)
+      assert(lps.continuousValues.head._1=='a')
+      assert(lps.continuousValues.head._2==0.5)
+      assert(lps.continuousValues.last._1=='b')
+      assert(lps.continuousValues.last._2==1.0)
+      assert(lps.letter(-123)=='a')
+      assert(lps.letter(0)=='a')
+      assert(lps.letter(0.2)=='a')
+      assert(lps.letter(0.5)=='a')
+      assert(lps.letter(0.6)=='b')
+      assert(lps.letter(1)=='b')
+      assert(lps.letter(123)=='b')
+
+      lps.add("xyz")
+      assert(lps.size==5)
+      lps.computeValues()
+      assert(lps.normalizedValues.size==5)
+      assert(lps.normalizedValues.head._1=='a')
+      assert(lps.normalizedValues.head._2==0.2)
+      assert(lps.normalizedValues.last._1=='z')
+      assert(lps.normalizedValues.last._2==0.2)
+      assert(lps.continuousValues.size==5)
+      assert(lps.continuousValues.head._1=='a')
+      assert(lps.continuousValues.head._2==0.2)
+      println(lps.normalizedValues)
+      println(lps.continuousValues)
+      println(lps.continuousValues.last)
+      assert(lps.continuousValues.last._1=='z')
+      assert(lps.continuousValues.last._2==1.0)
+      assert(lps.letter(-123)=='a')
+      assert(lps.letter(0)=='a')
+      assert(lps.letter(0.2)=='a')
+      assert(lps.letter(0.5)=='x')
+      assert(lps.letter(0.6)=='x')
+      assert(lps.letter(1)=='z')
+      assert(lps.letter(123)=='z')
     } catch {
       case e: Exception => println(e)
     }
-    println(monkey.letterWeightMap)
-    println(monkey.weightArray.toString)
-    assert(1==1)
+    println(lps.normalizedValues)
+    println(lps.continuousValues)
   }
 }
