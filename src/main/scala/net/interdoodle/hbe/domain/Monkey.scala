b6f446f5890dcd98d1b5704da6bbd50bdbcf862e
Mike Slinn <mslinn@micronauticsresearch.com>
2011-10-18 21:24:51 -0700

    ScalaTest still bogus

--- src/main/scala/net/interdoodle/hbe/domain/Monkey.scala
+++ src/main/scala/net/interdoodle/hbe/domain/Monkey.scala
@@ -6,10 +6,10 @@ import scala.Array
 /** Random or semi-random typist
  * @author Mike Slinn */
 
-class Monkey(letters:IndexedSeq[Char]=('a' to 'z') ++ ('a' to 'z') ++ ('0' to '9') /*++ ('.>,</?'";:[{]}\|=+-_\)\(*&^%$#@!~`')*/,
-             weights:Array[Double]=Array[Double]()) {
+class Monkey(letters:IndexedSeq[Char]=('A' to 'Z') ++ ('a' to 'z') ++ ('0' to '9') /*++ ('.>,</?'";:[{]}\|=+-_\)\(*&^%$#@!~`')*/,
+             weights:Array[Double]=new Array[Double](0)) {
   var letterWeightMap = new HashMap[Char, Double]
-  var weightArray:Array[Double] = weights
+  var weightArray = weights
   if (weights.length==0) { // all letters should have equal weight
     for (i <- 0 to  letters.length)
       weightArray(i) = 1.0 / letters.length;
--- src/test/scala/net/interdoodle/hbe/HelloBlueEyesSuite.scala
+++ src/test/scala/net/interdoodle/hbe/HelloBlueEyesSuite.scala
@@ -9,13 +9,15 @@ class HelloBlueEyesSuite extends FunSuite {
   var monkey:Monkey = _
 
   test("default constructor values") {
+    val letters:IndexedSeq[Char] = ('A' to 'Z') ++ ('a' to 'z') ++ ('0' to '9')
+    val weights:Array[Double] = new Array[Double](letters.length)
     try {
-      monkey = new Monkey()
+      monkey = new Monkey(letters, weights)
     } catch {
       case e: Exception => println(e)
     }
     println(monkey.letterWeightMap)
-    println(monkey.weightArray)
+    println(monkey.weightArray.toString)
     assert(1==1)
   }
 }
